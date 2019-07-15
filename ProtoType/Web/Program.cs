8b42f8edd27e1456b553d4ef797c2e54e9cfc70e
dp140719 <52877500+dp140719@users.noreply.github.com>
2019-07-15 15:55:07 +0300

    Add files via upload

---
+++ ProtoType/Web/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoTypeWeb
+{
+    class Program
+    {
+        static public string FormatBytes(long bytes)
+        {
+            string[] magnitudes =
+                new string[] { "GB", "MB", "KB", "Bytes" };
+            long max =
+                (long)Math.Pow(1024, magnitudes.Length);
+
+            return string.Format("{1:##.##} {0}",
+                magnitudes.FirstOrDefault(
+                    magnitude =>
+                    bytes > (max /= 1024)) ?? "0 Bytes",
+              (decimal)bytes / (decimal)max);
+        }
+
+
+        private static async Task WriteWebRequestSizeAsync(string url)
+        {
+            try
+            {
+                WebRequest webRequest =
+                    WebRequest.Create(url);
+                WebResponse response =
+                    await webRequest.GetResponseAsync();
+                using (StreamReader reader =
+                    new StreamReader(
+                        response.GetResponseStream()))
+                {
+                    string text =
+                        await reader.ReadToEndAsync();
+                    Console.WriteLine(
+                        FormatBytes(text.Length));
+                }
+            }
+            catch (WebException)
+            {
+                // ...
+            }
+            catch (IOException)
+            {
+
+                // ...
+            }
+            catch (NotSupportedException)
+            {
+                // ...
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string url = "http://www.IntelliTect.com";
+
+            Console.Write(url);
+
+            Task task = WriteWebRequestSizeAsync(url);
+
+            while (!task.Wait(100))
+            {
+                Console.Write(".");
+            }
+            // ...
+        }
+    }
+}
